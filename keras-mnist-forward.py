58bf7f4b0f2587baccb1defb2d69ae9f569309cf
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2016-10-28 17:20:43 +0100

    updates

--- keras-mnist-forward.py
+++ keras-mnist-forward.py
@@ -1,3 +1,4 @@
+import sys
 from keras.models import load_model
 from scipy.misc import imread
 
@@ -5,9 +6,14 @@ from scipy.misc import imread
 model = load_model('bettercnn.h5')
 
 # load an image
-image = imread('7.PNG').astype(float)
+image = imread(sys.argv[1]).astype(float)
 
 # normalise it in the same manner as we did for the training data
 image = image / 255.0
 
-print model.predict(image)
+#reshape
+image = image.reshape(1,1,28,28)
+
+# forward propagate and print index of most likely class 
+# (for MNIST this corresponds one-to-one with the digit)
+print model.predict_classes(image)[0]
